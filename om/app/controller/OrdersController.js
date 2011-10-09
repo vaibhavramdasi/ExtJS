b0b3d6168f9dbc09748c62b87bcb41ea9176c716
Vaibhav <ramdasivaibhav@gmail.com>
2011-10-09 12:00:00 +0300

    skinny controller approach

--- om/app/controller/OrdersController.js
+++ om/app/controller/OrdersController.js
@@ -3,7 +3,6 @@
  * @description controls OM grid .
  * @namespace OM.controller.OrdersController
  * @author Vaibhav Ramdasi
- * @last updated 10/20/2011
  */
 
 
@@ -15,68 +14,34 @@ Ext.define('OM.controller.OrdersController',
 	views:['order.List','Viewport','customer.Details'],			//Specify views to be used
 	refs:
 	[
-		{ref:'displayPanel',selector:'panel[itemId=panelCustomerInfo]'}   //Provide getters to components
+		
 	],
+
 	init:function()								
 		{
-			Ext.create('OM.view.Viewport'); 	//Creates viewport
-			
-			var panel=this.getDisplayPanel();
-			panel.removeAll();
-			panel.add(  						//Dynamically embed view to viewport 
-						{
-							xtype:'customerinfo',
-							flex:1,
-							minHeight:100,
-							autoHeight:true
-							
-						});
-						
-						
+										
 			this.control						//Setup Event handlers to Ext components
-				({
-				
-				'orderlist':
+				(
 					{
-						itemclick:this.getCustomerDetails
-					}
 					
-				});
+					'orderlist':
+						{
+							itemclick:this.getCustomerDetails
+						}
+						
+					}
+				);
 				
 		},
-	
+
+	onLaunch:function()
+		{
+			//manipulate views
+		},
 	getCustomerDetails:function(view,record, item,index,e,eOpts )
 		{
 			
-			
-			var cid=record.data.customerID;
-			  
-                 
-			
-			//Fetch Customer details
-			
-			Ext.Ajax.request({
-				
-				url:'services/GetCustomerDetails.php?cid='+cid+'',
-				method:'GET',
-				headers:'application/json; charset=utf-8',
-				success:function(result,request)
-				{
-					
-					var cidata=Ext.JSON.decode(result.responseText);
-					var rec=[Ext.create('OM.model.Customer',cidata.data)]; 
-					Ext.data.StoreManager.lookup('CustomerStore').loadRecords(rec);
-						
-													
-						
-					
-				},
-				failure:function(result,request){
-					//Error Handling
-					
-				}
-				
-			});
+			this.getCustomerStoreStore().load({params:{'cid':record.data.customerID}});
 			
 		}	
 		
